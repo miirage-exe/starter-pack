fd7d7f8f68544e1a65fbd83aae29a38cc62525bc
miirage <mika.baeza@laposte.net>
2022-08-17 11:13:57 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Project's Title :
+...
+
+Overview description :
+...
+
+What problem does this app solve?
+...
+
+How this app solves it (what this application does) ?
+...
+
+Technologies used :
+**Backend :**
+- node
+- ...
+**Frontend :**
+- html, css, js
+- ...
+
+**Optional** Why did I use *technologie name* ? **Repeat if needed**
+
+
+Features :
+- ...
+- ...
+- ...
+
+Credits :
+*desc* :
+by *author*, that you can find here : *source*
+
+License :
+https://creativecommons.org/choose/
---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const express = require('express')
+const http = require('http')
+const session = require('express-session')
+const port = process.env.PORT
+const { randomUUID } = require('crypto');
+
+const app = express()
+const server = http.createServer(app)
+
+// Define the public directory
+app.use(express.static('public'));
+// Define the view engine
+app.set('view engine', 'ejs')
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+/* <---- DELETE TO USE SESSION MIDDLEWARE
+
+// Use session middleware
+const sessionMiddleware = session({
+    secret: randomUUID(),
+    saveUninitialized: true,
+    resave: true,
+    extended: true,
+    cookie: { secure: false }})
+app.use(sessionMiddleware)
+
+DELETE TO USE SESSION MIDDLEWARE -----> */
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// Start the server
+server.listen(port, () => {
+    console.log('App listening on port ' + port)
+})
